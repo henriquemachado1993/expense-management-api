ec6dcd169886ae1dc4f96f4e76ad9798394a9246
Henrique machado <henrique.santos@terceiroslinx.com.br>
2023-12-24 09:43:06 -0300

    Melhorias no middleware que captura as exceptions para salvar só mensagem que interessa.

--- ExpenseApi/ExpenseApi.Infra/Middlewares/ExceptionMiddleware.cs
+++ ExpenseApi/ExpenseApi.Infra/Middlewares/ExceptionMiddleware.cs
@@ -7,6 +7,8 @@ using ExpenseApi.Infra.Context;
 using System;
 using System.Threading.Tasks;
 using ExpenseApi.Domain.ValueObjects;
+using ExpenseApi.Domain.Enums;
+using System.Net;
 
 namespace ExpenseApi.Infra.Middlewares
 {
@@ -28,39 +30,43 @@ namespace ExpenseApi.Infra.Middlewares
             catch (Exception ex)
             {
                 // grava no mongoDB
-                LogExceptionToMongoDB(ex, mongoDBContext);
+                await LogExceptionToMongoDB(mongoDBContext, ex);
 
                 // Devolve um erro amigável pro usuário
-                await HandleExceptionAsync(context, ex.Message);
+                await HandleExceptionAsync(context, ex);
             }
         }
 
-        private void LogExceptionToMongoDB(Exception ex, MongoDBContext mongoDBContext)
+        private static Task LogExceptionToMongoDB(MongoDBContext mongoDBContext, Exception ex)
         {
-            var auditLog = new AuditLog()
-            {
-                Id = Guid.NewGuid(),
-                Message = ex.Message,
-                Type = "error"
-            };
-            auditLog.SetDataFromObject(ex);
+            var auditLog = new AuditLog(Guid.NewGuid(), MessageType.Error.ToString(), ex.Message, DateTime.Now, GetDataByException(ex));
+            
             var collection = mongoDBContext.Database.GetCollection<AuditLog>(typeof(AuditLog).Name);
-            collection.InsertOne(auditLog);
+            return collection.InsertOneAsync(auditLog);
         }
 
-        private static Task HandleExceptionAsync(HttpContext context, string message)
+        private static Task HandleExceptionAsync(HttpContext context, Exception ex)
         {
             // Configurar a resposta HTTP com um status code específico e o detalhe do erro
             context.Response.ContentType = "application/json";
             context.Response.StatusCode = 500; // Internal Server Error
 
-            var response = ServiceResult<object>.CreateInvalidResult(message);
+            var response = ServiceResult<object>.CreateInvalidResult(GetDataByException(ex), ex.Message, HttpStatusCode.InternalServerError);
 
             // Serializa a resposta em JSON
             var jsonResponse = JsonConvert.SerializeObject(response);
 
             // Escreve a resposta no corpo da resposta HTTP
             return context.Response.WriteAsync(jsonResponse);
+        }
+
+        private static object GetDataByException(Exception ex)
+        {
+            return new
+            {
+                Message = ex.Message,
+                StackTraceString = ex.StackTrace
+            };
         }
     }
 }
